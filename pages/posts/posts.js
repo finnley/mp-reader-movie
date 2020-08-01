6f95a522e421154e1c51d6084224d09d143035aa
finnley <finnley.yuan@jingdigital.com>
2020-08-01 09:33:52 +0800

    post-item-template

--- pages/posts/posts.js
+++ pages/posts/posts.js
@@ -1,4 +1,6 @@
 // pages/posts/posts.js
+var postsData = require('../../data/posts-data.js')
+
 Page({
 
   /**
@@ -12,29 +14,10 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var posts_content = [{
-      date: "Nov 18 2019",
-      title: "正是虾肥蟹壮时",
-      post_img: "/images/post/crab.png",
-      author_img: '/images/avatar/1.png',
-      content: "菊黄蟹正肥，品尝秋之味。徐志摩把,“看初花的荻芦”和“到楼外楼吃蟹”,并列为秋天来杭州不能错过的风雅之事；用林妹妹的话讲是“螯封嫩玉双双满，",
-      collect_num: '96',
-      view_num: '123',
-    }, {
-      title: "比利·林恩的中场故事",
-        content: "一 “李安是一位绝不会重复自己的导演，本片将极富原创性李安众所瞩目的新片《比利林恩漫长的中场休息》，正式更名《半场无战事》。",
-        post_img: "/images/post/bl.png",
-        view_num: 62,
-        detail: "一 “李安是一位绝不会重复自己的导演，本片将极富原创性”李安众所瞩目的新片《比利林恩漫长的中场休息》，正式更名《半场无战事》。预告片首次曝光后，被视作是明年奥斯卡种子选手。该片根据同名畅销书改编。原著小说荣获美国国家图书奖。也被BBC评为21世纪最伟大的12本英文小说之一。影片讲述一位19岁德州男孩的比利·林恩入伍参加伊战，在一次交火中他大难不死，意外与战友成为大众的关注焦点，并被塑造成英雄。之后他们返回国内，在橄榄球赛中场休息时授勋。这名战争英雄却面临前所未有的心灵煎熬……李安为什么选中这部电影来拍？因为李安想要挑战前所未有的技术难题：以120帧每秒的速度、4K、3D技术全面结合，来掀起一场电影视觉革命。什么意思？所谓“电影是24格每秒的谎言”，其中的24格，就是帧数。",
-        collect_num: 92,
-        dateTime: "24小时前",
-        author: "迷的城",
-        date: "Nov 20 2016",
-        author_img: "/images/avatar/2.png",
-    }]
     // this.setData(posts_content) //没有数据，转换为对象
     this.setData({
-      posts_key: posts_content
+      // posts_key: posts_content
+      posts_key: postsData.postList
     })
   },
 
