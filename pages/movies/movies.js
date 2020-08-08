8f68a82980d92e787eeaa20644e2a0887832bc93
finnley <finnley.yuan@jingdigital.com>
2020-08-08 18:09:54 +0800

    movies page

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,18 +1,77 @@
 // pages/movies/movies.js
+var util = require('../../utils/util.js')
+
+var app = getApp();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    inTheaters: {},
+    comingSoon: {},
+    top250: {},
+    searchResult: {},
+    containerShow: true,
+    searchPanelShow: false,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var inTheatersUrl = app.globalData.doubanBase + "/v2/movie/in_theaters" + "?start=0&count=3";
+    var comingSoonUrl = app.globalData.doubanBase + "/v2/movie/coming_soon" + "?start=0&count=3";
+    var top250Url =  app.globalData.doubanBase + "/v2/movie/top250" + "?start=0&count=3";
+    
+    this.getMovieListData(inTheatersUrl, "inTheaters", "正在热映");
+    this.getMovieListData(comingSoonUrl, "comingSoon", "即将上映");
+    this.getMovieListData(top250Url, "top250", "豆瓣Top250");
+  },
 
+  getMovieListData: function(url, settedKey, categoryTitle) {
+    var that = this;
+    wx.request({
+      url: url,
+      method: 'GET',
+      header: {
+        "Content-Type": ""
+      },
+      success: function(res) {
+        console.log(res);
+        that.processDoubanData(res.data, settedKey, categoryTitle)
+      },
+      fail: function(error) {
+      }
+    })
+  },
+
+  processDoubanData: function(moviesDouban, settedKey, categoryTitle) {
+    var movies = [];
+    for (var idx in moviesDouban.subjects) {
+      var subject = moviesDouban.subjects[idx];
+      var title = subject.title;
+      if (title.length >= 6) {
+        title = title.substring(0, 6) + "...";
+      }
+
+      // [1,1,1,1,1] [1,1,1,0,0]
+      var temp = {
+        title: title,
+        average: subject.rating.average,
+        coverageUrl: subject.images.large,
+        movieId: subject.id,
+        stars: util.convertToStarsArray(subject.rating.stars),
+      };
+      movies.push(temp)
+    }
+    var readyData = {};
+    readyData[settedKey] = {
+      categoryTitle: categoryTitle,
+      movies: movies
+    }
+    this.setData(readyData);
   },
 
   /**
